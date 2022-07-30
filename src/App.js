e9d2ab85066997f3600a7b504e8da9069c461d39
dom <domalbano35@gmail.com>
2022-07-30 17:26:02 -0400

    working in the invoices tab

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper!</h1>
+      <Link to={'/'}><h1>Bookkeeper!</h1></Link>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
+    
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
     </div>
   );
 }
